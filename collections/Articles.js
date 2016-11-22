43b9db088f55ecae57f7775d34e269f0a0749079
Andy <abeaudoin2013@gmail.com>
2016-11-22 14:07:35 -0500

    added schema and simple form

--- collections/Articles.js
+++ collections/Articles.js
@@ -1,4 +1,18 @@
-Articles = new Meteor.Collection('articles');
+Articles = new Mongo.Collection('articles');
+
+// specify rules for who can insert into articles
+Articles.allow({
+	insert: function (userId, doc) {
+		// if userId exists, then you can add article
+		return !!userId;
+	}
+});
+
+Tag = new SimpleSchema({
+	name: {
+		type: String
+	}
+});
 
 ArticleSchema = new SimpleSchema({
 	title: {
@@ -15,13 +29,28 @@ ArticleSchema = new SimpleSchema({
 		// gives default val
 		autoValue: function () {
 			return this.userId;
+		},
+		autoform: {
+			type: "hidden"
 		}
-	}, 
+	},
+	onFrontPage: {
+		type: Boolean,
+		defaultValue: true,
+		optional: true,
+	},
+	tags: {
+		// [subDoc] makes incrementing fields
+		type: [Tag]
+	},
 	createdAt: {
 		type: Date,
 		label: "Created At",
 		autoValue: function () {
 			return new Date();
+		},
+		autoform: {
+			type: "hidden"
 		}
 	}
 });
